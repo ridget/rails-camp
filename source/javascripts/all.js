00e4369ec5523e99d54e80f63b65951766d6f693
Lauren <lauren.hennessy.work@gmail.com>
2016-03-03 19:58:33 +1030

    bits and bobs

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,15 @@
 //= require_tree .
+
+var map;
+function initMap() {
+  var mapDiv = document.getElementById('map');
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.986682, lng: 138.691938},
+    zoom: 10
+  });
+
+  var marker = new google.maps.Marker({
+    position: {lat: -35.0655083, lng: 138.75600800000007},
+    map: map
+  });
+}
