64d665be8a31734e1bd161baf383fe6e1cb49568
Adam Davies <adzdavies@gmail.com>
2016-05-07 15:51:55 +0930

    Refactor js up and tidy up form
    Problem in fields not focussing on click,
    but you can tab into them.
    
    Not sure on this... maybe needs some bootstrap js?

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -17,66 +17,47 @@ function initMap() {
 }
 
 
-// ***************************************************************
-// @author = Abhinav Rana, abhinav.rana1990@gmail.com
-// @date = 13 Feb, 2015
-//  @version = 0.1
-//
-// *************************************************************************************************************************
-// functions: a sample code to store data from html form to google sheet
-// *************************************************************************************************************************
 jQuery( document ).ready(function( $ ) {
-	// variable to hold request
-	var request;
-	// bind to the submit event of our form
-	$("#foo").submit(function(event){
-		// abort any pending request
-		if (request) {
-			request.abort();
-		}
-		// setup some local variables
-		var $form = $(this);
-		// let's select and cache all the fields
-		var $inputs = $form.find("input, select, button, textarea");
-		// serialize the data in the form
-		var serializedData = $form.serialize();
+  var $form = $('#sponsorship-form');
+  var gappsUrl = "https://script.google.com/macros/s/AKfycbxzteFiAyW9BubEPux_pNE6yx_fKE2MB21Sz4tPpbMGuPPAJurX/exec"
 
-		// let's disable the inputs for the duration of the ajax request
-		// Note: we disable elements AFTER the form data has been serialized.
-		// Disabled form elements will not be serialized.
-		$inputs.prop("disabled", true);
-		$('#result').text('Sending data...');
+  // Don't fire if no form on page
+  if ($form.length === 0) return
+
+  // Helper functions
+  // submit the form and return a promise
+  function submitForm(serializedData) {
+    return $.ajax({ url: gappsUrl, type: "post", data: serializedData });
+  }
+
+  function showResult(result) {
+    $('#result').html(result)
+  }
+
+  function enableForm(enabled) {
+    $form
+      .find("input, select, button, textarea")
+      .prop("disabled", !enabled);
+  }
+
+	$form.submit(function(event){
+		event.preventDefault();
+
+		var serializedData = $form.serialize();
+    enableForm(false);
+    showResult('Sending data...');
 
 		// fire off the request to /form.php
-		request = $.ajax({
-			url: "https://script.google.com/macros/s/AKfycbxzteFiAyW9BubEPux_pNE6yx_fKE2MB21Sz4tPpbMGuPPAJurX/exec", //url to web app created by us from our Google spreadsheet
-			type: "post",
-			data: serializedData
-		});
-
-		// callback handler that will be called on success
-		request.done(function (response, textStatus, jqXHR){
-			// log a message to the console
-			$('#result').html('<a href="https://docs.google.com/spreadsheets/d/1mxswRx4Af4yKxj5sRDUdjvRkgl-ggcEhhv0SpPyyyN4/edit#gid=0" target="_blank">Success - see Google Sheet</a>');
-			console.log("Hooray, it worked!");
-		});
-
-		// callback handler that will be called on failure
-		request.fail(function (jqXHR, textStatus, errorThrown){
-			console.error(
-				"The following error occured: "+
-				textStatus, errorThrown
-			);
-		});
-
-		// callback handler that will be called regardless
-		// if the request failed or succeeded
-		request.always(function () {
-			// reenable the inputs
-			$inputs.prop("disabled", false);
-		});
-
-		// prevent default posting of form
-		event.preventDefault();
+    submitForm(serializedData)
+      .done(function() {
+        showResult("Thanks for your submission!");
+      })
+      .fail(function(jqXHR, textStatus, errorThrown) {
+        showResult("Sorry, but something went wrong...");
+        console.error("The following error occured: "+ textStatus, errorThrown);
+		  })
+		  .always(function () {
+        enableForm(true);
+      });
 	});
 });
